da5c02734d56fd155f438ebbfbb7bedec46e97e7
EM <eldar.mussagali@gmail.com>
2025-04-25 10:15:27 +0500

    Enable cors for all

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
 import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.enableCors({
-    origin: configService.get<string>('FRONTEND_URL'),
+    origin: '*',
     credentials: true,
   });
   await app.listen(8000);
